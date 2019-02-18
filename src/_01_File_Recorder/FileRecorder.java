3bcac297bf66c14ceb56a7e8e30343bd83a0fd29
Sasanka742 <league@WTS20.attlocal.net>
2019-02-17 17:24:54 -0800

    started

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,19 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/magic");
+			fw.write("bwoiehfo");
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
